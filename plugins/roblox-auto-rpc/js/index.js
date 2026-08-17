41431807bd57abc0873b69d3d45b80d12965b8b2
Jzsnipr <jensonaltemail1@gmail.com>
2026-08-17 18:50:58 +0100

    Update index.js

--- plugins/roblox-auto-rpc/js/index.js
+++ plugins/roblox-auto-rpc/js/index.js
@@ -1,9 +1,18 @@
-export default plugin({
+export default {
+    name: "Roblox Auto RPC",
+    description: "Displays Roblox activity as Discord Rich Presence.",
+    authors: [
+        {
+            name: "Jzsnipr",
+            id: 0n,
+        },
+    ],
+
     start() {
-        console.log("[Roblox Auto RPC] started");
+        console.log("[Roblox Auto RPC] Started");
     },
 
     stop() {
-        console.log("[Roblox Auto RPC] stopped");
+        console.log("[Roblox Auto RPC] Stopped");
     },
-});
+};
